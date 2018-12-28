70027d4d33e90c846b1590a10478497532db4707
Luca Scotton <lucascott96@hotmail.it>
2018-12-28 15:53:05 +0100

    update create and load table

--- src/main/java/HBaseScrabble.java
+++ src/main/java/HBaseScrabble.java
@@ -7,12 +7,15 @@ import org.apache.hadoop.hbase.util.Bytes;
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 
 public class HBaseScrabble {
     private Configuration config;
     private HBaseAdmin hBaseAdmin;
     TableName table = TableName.valueOf("ScrabbleGames");
+    String primaryCf = "primaryCf";
+    String sideCf = "sideCf";
 
     /**
      * The Constructor. Establishes the connection with HBase.
@@ -28,26 +31,76 @@ public class HBaseScrabble {
     }
 
     public void createTable() throws IOException {
-        byte[] cf = Bytes.toBytes("BasicData");
 
         HTableDescriptor hTable = new HTableDescriptor(table);
-        HColumnDescriptor family = new HColumnDescriptor(cf);
-        family.setMaxVersions(10); // Default is 3.
-        hTable.addFamily(family);
-
+        hTable.addFamily(new HColumnDescriptor(primaryCf).setMaxVersions(10));
+        hTable.addFamily(new HColumnDescriptor(sideCf).setMaxVersions(10));
         this.hBaseAdmin.createTable(hTable);
     }
 
     public void loadTable(String folder)throws IOException{
+        // TODO: input as a file or a folder? ATM is a file.
         CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(new File(folder))));
         String[] nextRecord;
-        while ((nextRecord = csvReader.readNext()) != null) {
-            System.out.println("Name : " + nextRecord[0]);
-            System.out.println("Email : " + nextRecord[1]);
-            System.out.println("Phone : " + nextRecord[2]);
-            System.out.println("Country : " + nextRecord[3]);
-            System.out.println("==========================");
+        int count = 0;
+        while ((nextRecord = csvReader.readNext()) != null && count < 10) {
+            count++;
+            System.out.println(nextRecord);
         }
+    }
+
+    private void putAll(HTable hTable, int tourneyid, String winnername, int loserid, int winnerid, int gameid, String tie,
+                        int winnerscore, int winneroldrating, int winnernewrating, int winnerpos, int loseroldrating,
+                        int losernewrating, int loserpos, int round, int division, String date, String lexicon) throws InterruptedIOException, RetriesExhaustedWithDetailsException {
+
+        // primary column family
+        boolean bool_tie = tie.equals("True");
+        byte[] key = Bytes.toBytes(tourneyid + winnername);
+        byte[] byte_tourneyid = Bytes.toBytes(tourneyid);
+        byte[] byte_winnername = Bytes.toBytes(winnername);
+        byte[] byte_loserid = Bytes.toBytes(loserid);
+        byte[] byte_winnerid = Bytes.toBytes(winnerid);
+        byte[] byte_gameid = Bytes.toBytes(gameid);
+        byte[] byte_bool_tie = Bytes.toBytes(bool_tie);
+
+        // side column family
+        boolean bool_lexicon = lexicon.equals("True");
+        byte[] byte_winnerscore = Bytes.toBytes(winnerscore);
+        byte[] byte_winneroldrating = Bytes.toBytes(winneroldrating);
+        byte[] byte_winnernewrating = Bytes.toBytes(winnernewrating);
+        byte[] byte_winnerpos = Bytes.toBytes(winnerpos);
+        byte[] byte_loseroldrating = Bytes.toBytes(loseroldrating);
+        byte[] byte_losernewrating = Bytes.toBytes(losernewrating);
+        byte[] byte_loserpos = Bytes.toBytes(loserpos);
+        byte[] byte_round = Bytes.toBytes(round);
+        byte[] byte_division = Bytes.toBytes(division);
+        byte[] byte_date = Bytes.toBytes(date);
+        byte[] byte_bool_lexicon = Bytes.toBytes(bool_lexicon);
+
+        // Put command
+        Put put = new Put(key);
+        long ts = System.currentTimeMillis();
+
+        put.add(primaryCf.getBytes(), "tourneyid".getBytes(), ts, byte_tourneyid);
+        put.add(primaryCf.getBytes(), "winnername".getBytes(), ts, byte_winnername);
+        put.add(primaryCf.getBytes(), "loserid".getBytes(), ts, byte_loserid);
+        put.add(primaryCf.getBytes(), "winnerid".getBytes(), ts, byte_winnerid);
+        put.add(primaryCf.getBytes(), "gameid".getBytes(), ts, byte_gameid);
+        put.add(primaryCf.getBytes(), "bool_tie".getBytes(), ts, byte_bool_tie);
+
+        put.add(sideCf.getBytes(), "winnerscore".getBytes(), ts, byte_winnerscore);
+        put.add(sideCf.getBytes(), "winneroldrating".getBytes(), ts, byte_winneroldrating);
+        put.add(sideCf.getBytes(), "winnernewrating".getBytes(), ts, byte_winnernewrating);
+        put.add(sideCf.getBytes(), "winnerpos".getBytes(), ts, byte_winnerpos);
+        put.add(sideCf.getBytes(), "loseroldrating".getBytes(), ts, byte_loseroldrating);
+        put.add(sideCf.getBytes(), "losernewrating".getBytes(), ts, byte_losernewrating);
+        put.add(sideCf.getBytes(), "loserpos".getBytes(), ts, byte_loserpos);
+        put.add(sideCf.getBytes(), "round".getBytes(), ts, byte_round);
+        put.add(sideCf.getBytes(), "division".getBytes(), ts, byte_division);
+        put.add(sideCf.getBytes(), "date".getBytes(), ts, byte_date);
+        put.add(sideCf.getBytes(), "bool_lexicon".getBytes(), ts, byte_bool_lexicon);
+
+        hTable.put(put);
     }
 
     /**
