6f412282c1fc82a730a89c294db6c256bd4a7f55
Luca Scotton <lucascott96@hotmail.it>
2018-12-28 14:21:15 +0100

    First commit

---
+++ src/main/java/HBaseScrabble.java
@@ -0,0 +1,159 @@
+import com.opencsv.CSVReader;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class HBaseScrabble {
+    private Configuration config;
+    private HBaseAdmin hBaseAdmin;
+    TableName table = TableName.valueOf("ScrabbleGames");
+
+    /**
+     * The Constructor. Establishes the connection with HBase.
+     * @param zkHost
+     * @throws IOException
+     */
+    public HBaseScrabble(String zkHost) throws IOException {
+        config = HBaseConfiguration.create();
+        config.set("hbase.zookeeper.quorum", zkHost.split(":")[0]);
+        config.set("hbase.zookeeper.property.clientPort", zkHost.split(":")[1]);
+        HBaseConfiguration.addHbaseResources(config);
+        this.hBaseAdmin = new HBaseAdmin(config);
+    }
+
+    public void createTable() throws IOException {
+        byte[] cf = Bytes.toBytes("BasicData");
+
+        HTableDescriptor hTable = new HTableDescriptor(table);
+        HColumnDescriptor family = new HColumnDescriptor(cf);
+        family.setMaxVersions(10); // Default is 3.
+        hTable.addFamily(family);
+
+        this.hBaseAdmin.createTable(hTable);
+    }
+
+    public void loadTable(String folder)throws IOException{
+        CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(new File(folder))));
+        String[] nextRecord;
+        while ((nextRecord = csvReader.readNext()) != null) {
+            System.out.println("Name : " + nextRecord[0]);
+            System.out.println("Email : " + nextRecord[1]);
+            System.out.println("Phone : " + nextRecord[2]);
+            System.out.println("Country : " + nextRecord[3]);
+            System.out.println("==========================");
+        }
+    }
+
+    /**
+     * This method generates the key
+     * @param values The value of each column
+     * @param keyTable The position of each value that is required to create the key in the array of values.
+     * @return The encoded key to be inserted in HBase
+     */
+    private byte[] getKey(String[] values, int[] keyTable) {
+        String keyString = "";
+        for (int keyId : keyTable){
+            keyString += values[keyId];
+        }
+        byte[] key = Bytes.toBytes(keyString);
+
+        return key;
+    }
+
+
+
+    public List<String> query1(String tourneyid, String winnername) throws IOException {
+        //TO IMPLEMENT
+        System.exit(-1);
+        return null;
+
+    }
+
+    public List<String> query2(String firsttourneyid, String lasttourneyid) throws IOException {
+        //TO IMPLEMENT
+        System.exit(-1);
+        return null;
+    }
+
+    public List<String> query3(String tourneyid) throws IOException {
+        //TO IMPLEMENT
+        System.exit(-1);
+        return null;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        if(args.length<2){
+            System.out.println("Error: \n1)ZK_HOST:ZK_PORT, \n2)action [createTable, loadTable, query1, query2, query3], \n3)Extra parameters for loadTables and queries:\n" +
+                    "\ta) If loadTable: csvsFolder.\n " +
+                    "\tb) If query1: tourneyid winnername.\n  " +
+                    "\tc) If query2: firsttourneyid lasttourneyid.\n  " +
+                    "\td) If query3: tourneyid.\n  ");
+            System.exit(-1);
+        }
+        HBaseScrabble hBaseScrabble = new HBaseScrabble(args[0]);
+        if(args[1].toUpperCase().equals("CREATETABLE")){
+            hBaseScrabble.createTable();
+        }
+        else if(args[1].toUpperCase().equals("LOADTABLE")){
+            if(args.length!=3){
+                System.out.println("Error: 1) ZK_HOST:ZK_PORT, 2)action [createTables, loadTables], 3)csvsFolder");
+                System.exit(-1);
+            }
+            else if(!(new File(args[2])).isDirectory()){
+                System.out.println("Error: Folder "+args[2]+" does not exist.");
+                System.exit(-2);
+            }
+            hBaseScrabble.loadTable(args[2]);
+        }
+        else if(args[1].toUpperCase().equals("QUERY1")){
+            if(args.length!=4){
+                System.out.println("Error: 1) ZK_HOST:ZK_PORT, 2)query1, " +
+                        "3) tourneyid 4) winnername");
+                System.exit(-1);
+            }
+
+            List<String> opponentsName = hBaseScrabble.query1(args[2], args[3]);
+            System.out.println("There are "+opponentsName.size()+" opponents of winner "+args[3]+" that play in tourney "+args[2]+".");
+            System.out.println("The list of opponents is: "+Arrays.toString(opponentsName.toArray(new String[opponentsName.size()])));
+        }
+        else if(args[1].toUpperCase().equals("QUERY2")){
+            if(args.length!=4){
+                System.out.println("Error: 1) ZK_HOST:ZK_PORT, 2)query2, " +
+                        "3) firsttourneyid 4) lasttourneyid");
+                System.exit(-1);
+            }
+            List<String> playerNames =hBaseScrabble.query2(args[2], args[3]);
+            System.out.println("There are "+playerNames.size()+" players that participates in more than one tourney between tourneyid "+args[2]+" and tourneyid "+args[3]+" .");
+            System.out.println("The list of players is: "+Arrays.toString(playerNames.toArray(new String[playerNames.size()])));
+        }
+        else if(args[1].toUpperCase().equals("QUERY3")){
+            if(args.length!=3){
+                System.out.println("Error: 1) ZK_HOST:ZK_PORT, 2) query3, " +
+                        "3) tourneyid");
+                System.exit(-1);
+            }
+            List<String> games = hBaseScrabble.query3(args[2]);
+            System.out.println("There are "+games.size()+" that ends in tie in tourneyid "+args[2]+" .");
+            System.out.println("The list of games is: "+Arrays.toString(games.toArray(new String[games.size()])));
+        }
+        else{
+            System.out.println("Error: \n1)ZK_HOST:ZK_PORT, \n2)action [createTable, loadTable, query1, query2, query3], \n3)Extra parameters for loadTables and queries:\n" +
+                    "\ta) If loadTable: csvsFolder.\n " +
+                    "\tb) If query1: tourneyid winnername.\n  " +
+                    "\tc) If query2: firsttourneyid lasttourneyid.\n  " +
+                    "\td) If query3: tourneyid.\n  ");
+            System.exit(-1);
+        }
+
+    }
+
+
+
+}
