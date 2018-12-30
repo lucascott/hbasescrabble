100b57d8154bcd65d2057997f425c5bafe25a588
Luca Scotton <lucascott96@hotmail.it>
2018-12-30 13:56:21 +0100

    important fixes

--- src/main/java/HBaseScrabble.java
+++ src/main/java/HBaseScrabble.java
@@ -1,13 +1,18 @@
-import com.opencsv.CSVReader;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.*;
-import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.RetriesExhaustedWithDetailsException;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
+
 
 
 public class HBaseScrabble {
@@ -49,46 +54,63 @@ public class HBaseScrabble {
         File folder_file = new File(folder);
         File[] listOfFiles = folder_file.listFiles();
 
+        if (listOfFiles != null) {
+            System.out.println("[INFO] Reading " + listOfFiles[0].getName());
+            BufferedReader csvReader = new BufferedReader(new FileReader(listOfFiles[0]));
+            String[] header = csvReader.readLine().split(","); // skip first line
+            String line;
+            String[] nextRecord;
+            int c = 0;
+            while ((line = csvReader.readLine()) != null) {
+                nextRecord = line.split(",");
+                putAll(hTable, nextRecord[1], nextRecord[4], nextRecord[9], nextRecord[3], nextRecord[0], nextRecord[2],
+                        nextRecord[5],nextRecord[6],nextRecord[7],nextRecord[8],
+                        nextRecord[10],nextRecord[11],nextRecord[12],nextRecord[13], nextRecord[14],
+                        nextRecord[15],nextRecord[16],nextRecord[17],nextRecord[18]);
 
-        CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(listOfFiles[0])));
-        String[] nextRecord;
-        while ((nextRecord = csvReader.readNext()) != null) {
-            putAll(hTable, nextRecord[1], nextRecord[4], nextRecord[9], nextRecord[3], nextRecord[0], nextRecord[2],
-                    nextRecord[5],nextRecord[6],nextRecord[7],nextRecord[8],
-                    nextRecord[10],nextRecord[11],nextRecord[12],nextRecord[13],
-                    nextRecord[14],nextRecord[15],nextRecord[16],nextRecord[17]);
+                if (c%100 == 0) {
+                    System.out.println("##### Inserted line " + c);
+                    for (int i = 0; i < nextRecord.length; i++) {
+                        System.out.println(header[i] + " : " + nextRecord[i]);
+                    }
+                }
+                c++;
+            }
+            System.out.println("[INFO] Inserted " + c + " lines.");
+        }
+        else {
+            System.out.println("[INFO] There are no files in the directory.");
         }
     }
 
     private void putAll(HTable hTable, String tourneyid, String winnername, String loserid, String winnerid, String gameid, String tie,
                         String winnerscore, String winneroldrating, String winnernewrating, String winnerpos,
-                        String loserscore, String loseroldrating, String losernewrating, String loserpos,
+                        String losername, String loserscore, String loseroldrating, String losernewrating, String loserpos,
                         String round, String division, String date, String lexicon) throws InterruptedIOException, RetriesExhaustedWithDetailsException {
 
         // primary column family
-        boolean bool_tie = tie.equals("True");
         byte[] key = Bytes.toBytes(tourneyid + winnername);
-        byte[] byte_tourneyid = Bytes.toBytes(tourneyid);
-        byte[] byte_winnername = Bytes.toBytes(winnername);
-        byte[] byte_loserid = Bytes.toBytes(loserid);
-        byte[] byte_winnerid = Bytes.toBytes(winnerid);
-        byte[] byte_gameid = Bytes.toBytes(gameid);
-        byte[] byte_bool_tie = Bytes.toBytes(bool_tie);
+        byte[] byte_tourneyid = tourneyid.getBytes();
+        byte[] byte_winnername = winnername.getBytes();
+        byte[] byte_loserid = loserid.getBytes();
+        byte[] byte_winnerid = winnerid.getBytes();
+        byte[] byte_gameid = gameid.getBytes();
+        byte[] byte_tie = tie.getBytes(); //Bytes.toBytes(tie.equals("True")? 1:0);
 
         // side column family
-        boolean bool_lexicon = lexicon.equals("True");
-        byte[] byte_winnerscore = Bytes.toBytes(winnerscore);
-        byte[] byte_winneroldrating = Bytes.toBytes(winneroldrating);
-        byte[] byte_winnernewrating = Bytes.toBytes(winnernewrating);
-        byte[] byte_winnerpos = Bytes.toBytes(winnerpos);
-        byte[] byte_loserscore = Bytes.toBytes(loserscore);
-        byte[] byte_loseroldrating = Bytes.toBytes(loseroldrating);
-        byte[] byte_losernewrating = Bytes.toBytes(losernewrating);
-        byte[] byte_loserpos = Bytes.toBytes(loserpos);
-        byte[] byte_round = Bytes.toBytes(round);
-        byte[] byte_division = Bytes.toBytes(division);
-        byte[] byte_date = Bytes.toBytes(date);
-        byte[] byte_bool_lexicon = Bytes.toBytes(bool_lexicon);
+        byte[] byte_winnerscore = winnerscore.getBytes();
+        byte[] byte_winneroldrating = winneroldrating.getBytes();
+        byte[] byte_winnernewrating = winnernewrating.getBytes();
+        byte[] byte_winnerpos = winnerpos.getBytes();
+        byte[] byte_losername = losername.getBytes();
+        byte[] byte_loserscore = loserscore.getBytes();
+        byte[] byte_loseroldrating = loseroldrating.getBytes();
+        byte[] byte_losernewrating = losernewrating.getBytes();
+        byte[] byte_loserpos = loserpos.getBytes();
+        byte[] byte_round = round.getBytes();
+        byte[] byte_division = division.getBytes();
+        byte[] byte_date = date.getBytes();
+        byte[] byte_lexicon = lexicon.getBytes(); //Bytes.toBytes(lexicon.equals("True")? 1:0);
 
         // Put command
         Put put = new Put(key);
@@ -99,12 +121,13 @@ public class HBaseScrabble {
         put.add(primaryCf.getBytes(), "loserid".getBytes(), ts, byte_loserid);
         put.add(primaryCf.getBytes(), "winnerid".getBytes(), ts, byte_winnerid);
         put.add(primaryCf.getBytes(), "gameid".getBytes(), ts, byte_gameid);
-        put.add(primaryCf.getBytes(), "bool_tie".getBytes(), ts, byte_bool_tie);
+        put.add(primaryCf.getBytes(), "tie".getBytes(), ts, byte_tie);
 
         put.add(sideCf.getBytes(), "winnerscore".getBytes(), ts, byte_winnerscore);
         put.add(sideCf.getBytes(), "winneroldrating".getBytes(), ts, byte_winneroldrating);
         put.add(sideCf.getBytes(), "winnernewrating".getBytes(), ts, byte_winnernewrating);
         put.add(sideCf.getBytes(), "winnerpos".getBytes(), ts, byte_winnerpos);
+        put.add(sideCf.getBytes(), "losername".getBytes(), ts, byte_losername);
         put.add(sideCf.getBytes(), "loserscore".getBytes(), ts, byte_loserscore);
         put.add(sideCf.getBytes(), "loseroldrating".getBytes(), ts, byte_loseroldrating);
         put.add(sideCf.getBytes(), "losernewrating".getBytes(), ts, byte_losernewrating);
@@ -112,7 +135,7 @@ public class HBaseScrabble {
         put.add(sideCf.getBytes(), "round".getBytes(), ts, byte_round);
         put.add(sideCf.getBytes(), "division".getBytes(), ts, byte_division);
         put.add(sideCf.getBytes(), "date".getBytes(), ts, byte_date);
-        put.add(sideCf.getBytes(), "bool_lexicon".getBytes(), ts, byte_bool_lexicon);
+        put.add(sideCf.getBytes(), "lexicon".getBytes(), ts, byte_lexicon);
 
         hTable.put(put);
     }
